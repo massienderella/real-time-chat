c835742ed69635be4512f13aca8181f1bc4039f5
Massienderella <massiel.ams@gmail.com>
2018-02-18 17:33:02 -0300

    ordenando archivo y sus rutas

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+
+      $(function () {
+        var socket = io('http://34.227.11.223:3000');
+        $('form').submit(function(){
+          socket.emit('chat message', $('#m').val());
+          $('#m').val('');
+          return false;
+        });
+        socket.on('chat message', function(msg){
+          $('#messages').append($('<li>').text(msg));
+          window.scrollTo(0, document.body.scrollHeight);
+        });
+      });
